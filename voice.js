89ef131a8300950ab40d4fcdf68dc38243c0fe00
William Chandra <williamchans57@gmail.com>
2021-05-10 20:25:10 +0700

    time counter

--- voice.js
+++ voice.js
@@ -14,17 +14,23 @@ const TOKEN = "JA5ACRJCVXTMZBSIQAHLT6TFDPLQ5D53";
 // Set your wake word
 const WAKE_WORD = "gizmo";
 var globalTime = new Date();
-
+var gameState = 'game menu';
+var answer = [];
+var answered = [];
+var objectList = ['dog', 'cat', 't0', 't1', 't2', 't3', 't4', 't5', 't6', 't7'];
 AFRAME.registerComponent('hello-world', {
   init: function () {
-    let scene = document.querySelector('a-scene');
-    objects = ['dog', 'cat']
-    objects.forEach(element => {
-      let object = createObject(element);
-      scene.append(object);
-    });
+    loadScene();
   }
 });
+
+const loadScene = () => {
+  let scene = document.querySelector('a-scene');
+  objectList.forEach(element => {
+    let object = createObject(element);
+    scene.append(object);
+  });
+}
 // Component to set error message when the Wit.ai token has not been updated
 AFRAME.registerComponent('error-message', {
   init: () => {
@@ -32,7 +38,6 @@ AFRAME.registerComponent('error-message', {
       let textEl = document.querySelector('#text-object');
       textEl.setAttribute("text", `value: UPDATE CODE WITH YOUR WIT.AI TOKEN`);
     }
-    globalTime = new Date();
   }
 });
 
@@ -53,14 +58,24 @@ AFRAME.registerComponent('voice-command', {
 
       if (latestUtterance.isFinal) {
         // Exit the function if the wake word was not triggered to respect user privacy
-        if (!transcript.includes(`hey ${WAKE_WORD}`)) {
+        if (!transcript.includes(`hey ${WAKE_WORD}`) && gameState === 'game menu') {
           // Provide the user with a suggestion on voice commands they can say
           textEl.setAttribute("text", `value:Try saying: 'Hey ${WAKE_WORD}, Start game'`);
           return;
         }
+        else if (!transcript.includes(`difference`) && gameState === 'game play') {
+          // Provide the user with a suggestion on voice commands they can say
+          textEl.setAttribute("text", `value:Try saying: 'difference \"objects\"'`);
+          return;
+        }
 
         // Extract the utterance from the wake word
-        let utterance = transcript.split(`hey ${WAKE_WORD}`)[1];
+        let utterance = ''
+        if (gameState === 'game menu') {
+          utterance = transcript.split(`hey ${WAKE_WORD}`)[1];
+        } else if (gameState === 'game play') {
+          utterance = transcript;
+        }
 
         // Send the user's utterance to Wit.ai API for NLU inferencing
         fetch(`https://api.wit.ai/message?v=20210414&q=${utterance}`, {
@@ -70,9 +85,19 @@ AFRAME.registerComponent('voice-command', {
           .then(json => {
             // Add a 3D object to the scene based on the NLU inferencing result
             let scene = document.querySelector('a-scene');
-            let objectType = json["entities"]["object:object"][0].value;
-            let object = createObject(objectType);
-            scene.append(object);
+            entities = json["entities"]
+            if (entities["object:object"]) {
+              let objectType = entities["object:object"][0].value;
+              let object = createObject(objectType);
+              scene.append(object);
+            }
+            else if (entities["expression_game:expression_game"]) {
+              let game_setup = entities["expression_game:expression_game"][0].value;
+              if (game_setup == 'start') {
+                globalTime = new Date();
+                gameState = 'game play';
+              }
+            }
           });
       }
     };
@@ -91,7 +116,6 @@ function createObject(objectType) {
       break;
     case "cat":
       object.setAttribute("position", "3 1 -5");
-      // object.setAttribute('rotation', '0 45 45');
       object.setAttribute("scale", "0.1 0.1 0.1");
       object.setAttribute("gltf-model", '#cat');
       object.setAttribute('id', 'cat-entity');
